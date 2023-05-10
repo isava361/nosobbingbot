08002f7ba94a2e27c5321e805bf6b8fc453022b3
Ivan Savelev <iahito361@gmail.com>
2023-05-10 16:43:30 +0000

    Update file app.go

--- app.go
+++ app.go
@@ -36,7 +36,7 @@ func main() {
 				continue
 			} else {
 				text := m.Text
-				msg := tgbotapi.NewMessage(193117018, text)
+				msg := tgbotapi.NewMessage(85208468, text)
 				bot.Send(msg)
 			}
 		}
