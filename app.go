d14c22964fe243eb082ef547ce265ae33b29b121
Ivan Savelev <iahito361@gmail.com>
2023-05-12 11:14:08 +0000

    Update file app.go

--- app.go
+++ app.go
@@ -44,15 +44,57 @@ func main() {
 			} else if m.Chat.Type == "private" && m.Chat.ID != admin {
 				
 				chatID := encryptID(m.Chat.ID)
-
-				text := strconv.FormatInt(chatID, 10) + "\n" + m.Text
-				msg := tgbotapi.NewMessage(admin, text)
-				bot.Send(msg)
+				if m.Text != nil {
+					text := strconv.FormatInt(chatID, 10) + "\n" + m.Text
+					msg := tgbotapi.NewMessage(admin, text)
+					bot.Send(msg)
+				}else if m.Photo != nil {
+					text := chatID
+					existingFileID := m.Photo[len(m.Photo)-1].FileID
+					photoMsg := tgbotapi.NewPhoto(admin, tgbotapi.FileID(existingFileID))
+					photoMsg.Caption = text
+					bot.Send(photoMsg)
+				} else if m.Audio != nil {
+					text := chatID
+					existingFileID := m.Audio.FileID
+					audioMsg := tgbotapi.NewAudio(admin, tgbotapi.FileID(existingFileID))
+					audioMsg.Caption = text
+					bot.Send(audioMsg)
+				} else if m.Document != nil {
+					text := chatID
+					existingFileID := m.Document.FileID
+					documentMsg := tgbotapi.NewDocument(admin, tgbotapi.FileID(existingFileID))
+					documentMsg.Caption = text
+					bot.Send(documentMsg)
+				} else if m.Video != nil {
+					text := chatID
+					existingFileID := m.Video.FileID
+					videoMsg := tgbotapi.NewVideo(admin, tgbotapi.FileID(existingFileID))
+					videoMsg.Caption = text
+					bot.Send(videoMsg)
+				} else if m.Voice != nil {
+					text := chatID
+					msg := tgbotapi.NewMessage(admin, text)
+					existingFileID := m.Voice.FileID
+					voiceMsg := tgbotapi.NewVoice(admin, tgbotapi.FileID(existingFileID))
+					bot.Send(msg)
+					bot.Send(voiceMsg)
+				} else if m.VideoNote != nil {
+					text := chatID
+					msg := tgbotapi.NewMessage(admin, text)
+					existingFileID := m.VideoNote.FileID
+					length := m.VideoNote.Length
+					videoNoteMsg := tgbotapi.NewVideoNote(admin, length, tgbotapi.FileID(existingFileID))
+					bot.Send(msg)
+					bot.Send(videoNoteMsg)
+				}
 			} else if m.Chat.ID == admin && m.ReplyToMessage != nil {
 				originalmessage := m.ReplyToMessage
 				messagetext := originalmessage.Text
+				if m.Caption != nil {
+					messagetext = originalmessage.Text
+				}
 				words := strings.Fields(messagetext)
-					// Check if there's at least one word
 				if len(words) > 0 {
 					firstWord := words[0]
 					encryptedmessage, _ := strconv.ParseInt(string(firstWord), 10, 64)
@@ -61,6 +103,35 @@ func main() {
 					if m.Text != "" {
 						msg := tgbotapi.NewMessage(replychat, m.Text)
 						bot.Send(msg)
+					} else if m.Photo != nil {
+						existingFileID := m.Photo[len(m.Photo)-1].FileID
+						photoMsg := tgbotapi.NewPhoto(replychat, tgbotapi.FileID(existingFileID))
+						photoMsg.Caption = m.Caption
+						bot.Send(photoMsg)
+					} else if m.Audio != nil {
+						existingFileID := m.Audio.FileID
+						audioMsg := tgbotapi.NewAudio(replychat, tgbotapi.FileID(existingFileID))
+						audioMsg.Caption = m.Caption
+						bot.Send(audioMsg)
+					} else if m.Document != nil {
+						existingFileID := m.Document.FileID
+						documentMsg := tgbotapi.NewDocument(replychat, tgbotapi.FileID(existingFileID))
+						documentMsg.Caption = m.Caption
+						bot.Send(documentMsg)
+					} else if m.Video != nil {
+						existingFileID := m.Video.FileID
+						videoMsg := tgbotapi.NewVideo(replychat, tgbotapi.FileID(existingFileID))
+						videoMsg.Caption = m.Caption
+						bot.Send(videoMsg)
+					} else if m.Voice != nil {
+						existingFileID := m.Voice.FileID
+						voiceMsg := tgbotapi.NewVoice(replychat, tgbotapi.FileID(existingFileID))
+						bot.Send(voiceMsg)
+					} else if m.VideoNote != nil {
+						existingFileID := m.VideoNote.FileID
+						length := m.VideoNote.Length
+						videoNoteMsg := tgbotapi.NewVideoNote(replychat, length, tgbotapi.FileID(existingFileID))
+						bot.Send(videoNoteMsg)
 					}
 				}
 			} 
